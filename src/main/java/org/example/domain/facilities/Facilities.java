304a866db7047c61caf2d7aca6d8265e3d44d578
박종범 <84450816+asa9874@users.noreply.github.com>
2025-11-20 15:07:03 +0900

    feat : 부대시설 데이터 추가

--- src/main/java/org/example/domain/facilities/Facilities.java
+++ src/main/java/org/example/domain/facilities/Facilities.java
@@ -1,4 +1,43 @@
 package org.example.domain.facilities;
 
+import java.time.LocalDateTime;
+
 public class Facilities {
+    private int id;
+    private String name;
+    private LocalDateTime openingTime;
+    private LocalDateTime closingTime;
+    boolean requireReservation;
+
+    public Facilities(
+        int id,
+        String name,
+        LocalDateTime openingTime,
+        LocalDateTime closingTime,
+        boolean requireReservation
+    ) {
+        this.id = id;
+        this.name = name;
+        this.openingTime = openingTime;
+        this.closingTime = closingTime;
+        this.requireReservation = requireReservation;
+    }
+
+    public int getId() {
+        return id;
+    }
+    public String getName() {
+        return name;
+    }
+    public LocalDateTime getOpeningTime() {
+        return openingTime;
+    }
+
+    public LocalDateTime getClosingTime() {
+        return closingTime;
+    }
+    public boolean isRequireReservation() {
+        return requireReservation;
+    }
+
 }
