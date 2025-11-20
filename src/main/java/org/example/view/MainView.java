c5f2a52e37a4919a4769605da2c6c2c067c48493
ExistenceOne <121415293+ExistenceOne@users.noreply.github.com>
2025-11-20 14:02:35 +0900

    fix: 펜션 관리 버튼 복원

--- src/main/java/org/example/view/MainView.java
+++ src/main/java/org/example/view/MainView.java
@@ -24,19 +24,24 @@ public class MainView {
             cleaningStaffView.start(stage);
         });
 
+        // 펜션 관리 버튼
+        Button pensionButton = new Button("펜션 목록 검색");
+        pensionButton.setOnAction(e -> {
+            PensionView pensionView = new PensionView();
+            pensionView.start(stage);
+        });
+
         // 로그인 버튼
         Button loginButton = new Button("로그인");
         loginButton.setOnAction(e -> {
             LoginView loginView = new LoginView(stage);
             loginView.show();
         });
-        
-      // 레이아웃 설정
+        // 레이아웃 설정
         VBox vbox = new VBox(20);
         vbox.setPadding(new Insets(30));
         vbox.setAlignment(Pos.CENTER);
-        vbox.getChildren().addAll(cleaningStaffButton);
-        vbox.getChildren().addAll(loginButton);
+        vbox.getChildren().addAll(cleaningStaffButton, pensionButton, loginButton);
 
         // 씬 설정
         Scene scene = new Scene(vbox, 800, 600);
